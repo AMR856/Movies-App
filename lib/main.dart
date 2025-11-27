587d448220b8232eb7dfa2f37d7619095fbac06e
Amr Alnus <amer.live477@gmail.com>
2025-11-27 19:47:01 +0200

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ScreenUtilInit(
+      designSize: const Size(430, 932),
+      minTextAdapt: true,
+      splitScreenMode: true,
+      builder: (context, child) {
+        return MaterialApp(
+          title: 'Movies App',
+          debugShowCheckedModeBanner: false,
+          supportedLocales: const [Locale('en'), Locale('ar')],
+        );
+      },
+    );
+  }
+}
